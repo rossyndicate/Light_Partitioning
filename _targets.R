4517ebdfb10f00079b1f1d6499c66bfe047ead38
mbrousil <mbrousil@colostate.edu>
2023-01-12 17:49:52 -0800

    Fill out _targets.R with steps from 01_data_prep.Rmd

--- _targets.R
+++ _targets.R
@@ -84,5 +84,90 @@ list(
                 ecoregion_download %>%
                   grep(pattern = "shp$", value = TRUE),
                 st_read(dsn = !!.x),
-                packages = "sf")
+                packages = "sf"),
+  
+  
+  
+  # 2. Data management and eval ---------------------------------------------
+  
+  tar_target(inv_type_edit,
+             inv_type %>%
+               select(SiteID = MonitoringLocationIdentifier,
+                      type = ResolvedMonitoringLocationTypeName) %>%
+               mutate(type = ifelse(grepl('Lake',type),'Lake',type))),
+  
+  tar_target(site_vis,
+             site %>%
+               inner_join(inv_type_edit) %>%
+               distinct(SiteID,lat,long,type)),
+  
+  # Select only simultaneous observations
+  tar_target(simul,
+             # Only bother with data that has complete simultaneous observations of
+             # chl_a, doc, etc...
+             in_vis %>%
+               select(-p_sand) %>%
+               filter(if_all(c(chl_a,doc,tss,secchi), ~!is.na(.))) %>% 
+               inner_join(site_vis) %>%
+               filter(type != 'Facility') %>%
+               #Set some reasonable thresholds, AquaSat is too generous
+               filter(secchi < 15,
+                      chl_a < 1000,## ug/L
+                      tss < 1000, ## mg/L
+                      doc < 50),
+             format = "feather"),
+  
+  tar_target(no_secchi,
+             in_vis %>%
+               select(-p_sand) %>%
+               filter(across(c(chl_a,doc,tss), ~!is.na(.))) %>% 
+               inner_join(site_vis) %>%
+               filter(type != 'Facility') %>%
+               #Set some reasonable thresholds, AquaSat is too generous
+               filter(
+                 chl_a < 1000,## ug/L
+                 tss < 1000, ## mg/L
+                 doc < 50),
+             format = "feather"),
+  
+  tar_target(unique_simul,
+             simul %>%
+               distinct(SiteID, lat, long, type) %>%
+               st_as_sf(.,coords = c('long','lat'), crs = 4326),
+             packages = c("tidyverse", "sf")),
+  
+  # Where are sites with simultaneous observations of clarity constituents?
+  # Issues with the mapview package here right now
+  tar_target(simul_vis,
+             {
+               # Remove flat geobuff which breaks display
+               mapviewOptions(fgb = FALSE)
+               mapview(unique_simul, zcol = 'type')
+             },
+             packages = c("tidyverse", "sf", "mapview")),
+  
+  # What is the general relationship between variables in log log space
+  tar_target(log_simul_vis,
+             {
+               log_simul <- simul %>%
+                 dplyr::mutate(across(c(secchi,chl_a,tss,doc,tis),
+                                      log10)) %>%
+                 dplyr::filter(across(c(chl_a,doc,secchi,tss),
+                                      ~!is.na(.) & . < Inf & . > -Inf)) 
+
+               log_simul %>%
+                 sample_frac(0.2) %>%
+                 ungroup() %>%
+                 select(secchi,chl_a,tss,doc,type) %>%
+                 ggpairs(lower = list(continuous = wrap('points',shape = 1)),
+                         diag = list(continuous = wrap('densityDiag', alpha = 0.5)),
+                         mapping = ggplot2::aes(color = type),
+                         columns = c('secchi','chl_a','tss','doc')) +
+                 ggthemes::theme_few() + 
+                 scale_color_manual(values = c('seagreen3','skyblue3','saddlebrown'))
+             },
+             packages = c("tidyverse", "GGally", "ggthemes")
+  )
+  
+  
 )
